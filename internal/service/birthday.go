d54c21529b24a4083d979622253485f896d96dca
nugraha <nyperdana@gmail.com>
2024-08-23 16:46:40 +0700

    Fix : Remove unused code

--- internal/service/birthday.go
+++ internal/service/birthday.go
@@ -2,9 +2,6 @@ package service
 
 import (
 	"errors"
-	"math"
-	"slices"
-	"strings"
 	"time"
 )
 
@@ -57,87 +54,4 @@ func (s *ourService) GetBirthdayByID(userID uint32) (time.Time, error) {
 		return time.Time{}, err
 	}
 	return birthday, nil
-}
-
-func sortAlphabetical(a []string) []string {
-	for i := range a {
-		a[i] = strings.ToLower(a[i])
-		for j := range a {
-			a[i] = strings.ToLower(a[j])
-			if a[i] < a[j] {
-				a[i], a[j] = a[j], a[i]
-			}
-		}
-	}
-	return a
-}
-func mergeSortWithInputSorted(left, right []string) []string {
-	result := make([]string, 0, len(left)+len(right))
-	i, j := 0, 0
-	for i < len(left) && j < len(right) {
-		if left[i] <= right[j] {
-			result = append(result, left[i])
-			i++
-		} else {
-			result = append(result, right[j])
-			j++
-		}
-	}
-	result = append(result, left[i:]...)
-	result = append(result, right[j:]...)
-	return sortAlphabetical(result)
-}
-
-func merge(left, right []int) []int {
-	result := make([]int, 0, len(left)+len(right))
-	i, j := 0, 0
-
-	for i < len(left) && j < len(right) {
-
-		if left[i] <= right[j] {
-			result = append(result, left[i])
-			i++
-		} else {
-			result = append(result, right[j])
-			j++
-		}
-	}
-
-	result = append(result, left[i:]...)
-	result = append(result, right[j:]...)
-
-	slices.Sort(result)
-
-	return result
-}
-
-func factorial(n int) int {
-	if n < 0 {
-		return 0
-	}
-	if n == 0 || n == 1 {
-		return 1
-	}
-	return n * factorial(n-1)
-}
-
-// add test cases factorial
-
-func fibonacci(n int) int {
-	if n <= 1 {
-		return n
-	}
-	return fibonacci(n-1) + fibonacci(n-2)
-}
-
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
 }
